4f6585a0d157f80879fc05d8590b27da185d04cb
Fadlyyyy <fadlyyansah86@gmail.com>
2022-03-19 15:59:39 +0700

    message commit

--- database/migrations/2022_03_19_061529_create_detail_user_table.php
+++ database/migrations/2022_03_19_061529_create_detail_user_table.php
@@ -15,13 +15,14 @@ class CreateDetailUserTable extends Migration
     {
         Schema::create('detail_user', function (Blueprint $table) {
             $table->id();
-            $table->integer('type_user_id');
+            $table->foreignId('user_id')->nullable()->index('fk_detail_user_to_users');
+            $table->foreignId('type_user_id')->nullable()->index('fk_detail_user_to_type_user');
             $table->string('contact')->nullable();
             $table->longText('address')->nullable();
             $table->longText('photo')->nullable();
             $table->enum('gender', [1,2])->nullable();
             $table->timestamps();
-            $table->softDeletes(); //wajib ada ketika membuat create table
+            $table->softDeletes();
         });
     }
 
