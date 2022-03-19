960adf77c552fcfbeb43e9951277a8a14cf4caec
Fadlyyyy <fadlyyansah86@gmail.com>
2022-03-19 14:09:42 +0700

    message commit

---
+++ database/migrations/2022_03_19_064830_create_appointment_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('doctor_id');
+            $table->integer('user_id');
+            $table->integer('consultation_id');
+            $table->enum('level', [1,2,3]);
+            $table->date('date');
+            $table->time('time');
+            $table->enum('status', [1,2]);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointment');
+    }
+}
